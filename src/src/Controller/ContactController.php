55776f52ae6b1c595455251428c9a3aae9e3ff16
Dennis <dennis@Denniss-MacBook-Pro.local>
2024-03-09 08:29:51 +0100

    feat: install twig and add template

--- src/src/Controller/ContactController.php
+++ src/src/Controller/ContactController.php
@@ -4,12 +4,15 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ContactController
+class ContactController extends AbstractController
 {
     #[Route("/contact")]
     public function get(): Response
     {
-        return new Response("<h1>Contact</h1>");
+        return $this->render("contact/list.html.twig", [
+            "contacts" => [["first_name" => "Piet", "last_name" => "Friet"]],
+        ]);
     }
 }
