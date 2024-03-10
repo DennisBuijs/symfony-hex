4cabfa84213e62df5743836ce1aef97ab4e9044a
Dennis <dennis@Denniss-MacBook-Pro.local>
2024-03-10 17:47:39 +0100

    feat: implement sqlite repository

---
+++ src/src/Infrastructure/Contact/SqliteContactRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Infrastructure\Contact;
+
+use App\Domain\Contact\ContactRepository;
+use App\Domain\Contact\Contact;
+use App\Domain\Contact\ContactCollection;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\DBAL\Connection;
+
+class SqliteContactRepository implements ContactRepository
+{
+    private Connection $db;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->db = $entityManager->getConnection();
+    }
+
+    public function find(string $id): Contact
+    {
+        $query = $this->db->executeQuery('SELECT * FROM contacts WHERE id = "' . $id . '"');
+        $row = $query->fetchAssociative();
+
+        return new Contact(
+            $row['id'],
+            $row['first_name'],
+            $row['last_name'],
+            $row['email']
+        );
+    }
+
+    public function findAll(): ContactCollection
+    {
+        $query = $this->db->executeQuery('SELECT * FROM contacts');
+        $rows = $query->fetchAllAssociative();
+
+        $contactCollection = new ContactCollection();
+
+        foreach ($rows as $row) {
+            $contactCollection->add(new Contact(
+                $row['id'],
+                $row['first_name'],
+                $row['last_name'],
+                $row['email']
+            ));
+        }
+
+        return $contactCollection;
+    }
+    
+    public function update(string $id, Contact $contact): void
+    {
+        $this->db->executeStatement("
+            UPDATE contacts
+            SET first_name = ?, last_name = ?, email = ?, updated_at = CURRENT_TIMESTAMP
+            WHERE id = ?;
+        ", [
+            $contact->firstName,
+            $contact->lastName,
+            $contact->email,
+            $id
+        ]);
+    }
+}
