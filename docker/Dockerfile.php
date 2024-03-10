25477d5a654feefa4d7ee57a523143f13e9427c1
Dennis <dennis@Denniss-MacBook-Pro.local>
2024-03-10 17:20:07 +0100

    feat: install doctrine for sqlite

--- docker/Dockerfile.php
+++ docker/Dockerfile.php
@@ -1,7 +1,5 @@
 FROM php:8.3-fpm-alpine
 
-# Install necessary extensions
-RUN docker-php-ext-install pdo_mysql
+RUN docker-php-ext-install pdo_mysql sqlite3
 
-# Install Composer globally
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
